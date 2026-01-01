6ffd6b0c71072d8a1f797a558d832a716c9546cf
Kingestif <kingestiff@gmail.com>
2026-01-01 09:50:18 +0300

    hide password from response

--- auth_service/app/models.py
+++ auth_service/app/models.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
-    hashed_password: str
+    hashed_password: str = Field(exclude=True)
     age: Optional[int] = None
     gender: Optional[str] = None
     height: Optional[float] = None
