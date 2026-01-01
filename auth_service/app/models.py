bf5f593c72173be173c2b886e21e2bb818a7fbb7
Danielyilma <yilmad752@gmail.com>
2026-01-01 05:32:51 +0300

    update authtication service

--- auth_service/app/models.py
+++ auth_service/app/models.py
@@ -5,10 +5,20 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
     hashed_password: str
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    height: Optional[float] = None
+    weight: Optional[float] = None
 
 class UserCreate(SQLModel):
     username: str
     password: str
+
+class UserUpdate(SQLModel):
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    height: Optional[float] = None
+    weight: Optional[float] = None
 
 class Token(SQLModel):
     access_token: str
